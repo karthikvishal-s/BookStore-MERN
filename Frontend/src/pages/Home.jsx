4394889f0afc3b5527c818049f8328443afcee9a
Karthik Vishal <karthikvishal1506@gmail.com>
2025-05-11 23:35:40 +0530

    Deployment additions

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -34,15 +34,12 @@ const Home = () => {
   }, []);
 
   return (
-    <div className='bg-yellow-200 min-h-screen'>
+    <div className='bg-blue-200 min-h-screen'>
       <div className='flex justify-around items-center '>
-        <h1 className='text-3xl  my-8 text-red-600 font-bold'>
-          <center>Books List</center>
+        <h1 className='text-4xl  my-8 text-red-600 font-bold'>
+          <center><u>Books List</u></center>
         </h1>
-        <Link to='/books/create' className='bg-blue-500 text-2xl text-white px-4 py-2 rounded-lg flex items-center hover:text-blue-200'>
-          <MdOutlineAddBox className='align-center mr-2 text-3xl' />
-          Create Book
-        </Link>
+
       </div><br></br><br></br>
       {loading ? (
         <Spinner />
@@ -50,43 +47,48 @@ const Home = () => {
         <div className="text-red-500 text-center">{error}</div>
       ) : books && books.length > 0 ? (
         <div>
-        <table className=' border border-gray-300 justify-around min-w-screen items-center s'>
-          <thead>
-            <tr>
-              <th className='border px-2 py-2 text-2xl'>No</th>
-              <th className='border px-4 py-2 text-2xl'>Title</th>
-              <th className='border px-4 py-2 max-md:hidden text-2xl'>Author</th>
-              <th className='border px-2 py-2 max-md:hidden text-2xl'>Publish Year</th>
-              <th className='border px-4 py-2 text-2xl'>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {books.map((book, index) => (
-              <tr key={book._id} className='h-8 hover:bg-gray-100'>
-                <td className='border px-4 py-2'>{index + 1}</td>
-                <td className='border px-4 py-2'>{book.title}</td>
-                <td className='border px-4 py-2 max-md:hidden'>{book.author}</td>
-                <td className='border px-4 py-2 max-md:hidden text-center'>{book.publishYear}</td>
-                <td className='border px-4 py-2'>
-                  <div className='flex justify-center gap-x-4'>
-                    <Link to={`/books/details/${book._id}`} className='text-blue-500 hover:text-blue-800'>
-                      <BsInfoCircle className='text-2xl' />
-                    </Link>
-                    <Link to={`/books/edit/${book._id}`} className='text-green-500 hover:text-green-800'>
-                      <AiOutlineEdit className='text-2xl' />
-                    </Link>
-                    <Link to={`/books/delete/${book._id}`} className='text-red-500 hover:text-red-800'>
-                      <AiOutlineDelete className='text-2xl' />
-                    </Link>
-                  </div>
-                </td>
+          <table className=' border border-gray-300 justify-between  items-center  mx-auto w-full max-w-5xl'>
+            <thead>
+              <tr>
+                <th className='border px-2 py-2 text-2xl'>No</th>
+                <th className='border px-4 py-2 text-2xl'>Title</th>
+                <th className='border px-4 py-2 max-md:hidden text-2xl'>Author</th>
+                <th className='border px-2 py-2 max-md:hidden text-2xl'>Publish Year</th>
+                <th className='border px-4 py-2 text-2xl'>Actions</th>
               </tr>
-            ))}
-          </tbody>
-        </table></div>
+            </thead>
+            <tbody>
+              {books.map((book, index) => (
+                <tr key={book._id} className='h-8 hover:bg-gray-100'>
+                  <td className='border px-4 py-2'>{index + 1}</td>
+                  <td className='border px-4 py-2'>{book.title}</td>
+                  <td className='border px-4 py-2 max-md:hidden'>{book.author}</td>
+                  <td className='border px-4 py-2 max-md:hidden text-center'>{book.publishYear}</td>
+                  <td className='border px-4 py-2'>
+                    <div className='flex justify-center gap-x-4'>
+                      <Link to={`/books/details/${book._id}`} className='text-blue-500 hover:text-blue-800'>
+                        <BsInfoCircle className='text-2xl' />
+                      </Link>
+                      <Link to={`/books/edit/${book._id}`} className='text-green-500 hover:text-green-800'>
+                        <AiOutlineEdit className='text-2xl' />
+                      </Link>
+                      <Link to={`/books/delete/${book._id}`} className='text-red-500 hover:text-red-800'>
+                        <AiOutlineDelete className='text-2xl' />
+                      </Link>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table><br></br><br></br><br></br></div>
       ) : (
         <div className="text-center py-4">No books found. Add some books to see them here.</div>
-      )}
+      )}<div className='flex justify-center my-4'>
+
+        <Link to='/books/create' className='bg-blue-500 text-2xl text-white px-4 py-2 rounded-lg flex items-center hover:text-blue-200'>
+          <MdOutlineAddBox className='align-center mr-2 text-3xl' />
+          Create Book
+        </Link></div>
     </div>
   );
 };
