07a44f706f4483338096640d3bdd30a083c33f0c
Karthik Vishal <karthikvishal1506@gmail.com>
2025-05-12 09:23:56 +0530

    vercel.json

--- Frontend/src/pages/DeleteBooks.jsx
+++ Frontend/src/pages/DeleteBooks.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { useNavigate,useParams } from 'react-router-dom';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
+import { Link } from 'react-router-dom';
 
 
 const DeleteBooks = () => {
@@ -28,14 +29,17 @@ const DeleteBooks = () => {
 
   return (
     <div className='p-4'>
-      <BackButton/>
+      
      
       {loading ? <Spinner></Spinner>:" "}
       <div className='flex flex-col items-center border-2 border-sky-600 rounded-xl w-[600px] p-8 mx-auto'>
         <h3 className='text-2xl'>Are you sure you want to delete this book?</h3>
-        <button className='p-4 bg-red-600 text-white m-8 w-full' onClick={handleDelete}>
+        <button className='p-4 bg-red-600 text-white m-8 w-full text-2xl' onClick={handleDelete}>
           Yes,Delete it
         </button>
+        <Link className='p-4 bg-blue-600 text-white m-8 w-full text-center text-2xl' to={"/"}>
+          No,Go Back
+        </Link>
       </div>
           </div>
   )
