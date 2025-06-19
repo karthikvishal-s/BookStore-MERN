539f56045830df0e6aad6c1e53fefadef37b4cc0
Karthik Vishal <karthikvishal1506@gmail.com>
2025-06-19 21:55:43 +0530

    final push

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 
 
 
-export const PORT = process.env.PORT || 'https://pensieve-karthik.vercel.app';
+export const PORT = process.env.PORT || 'https://thepensieve.vercel.app';
 
 // Use environment variable in production, fallback for development
 export const mongoDBURL = process.env.MONGODB_URL || 'mongodb+srv://root:root@bookstore.eq0hudx.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore';
