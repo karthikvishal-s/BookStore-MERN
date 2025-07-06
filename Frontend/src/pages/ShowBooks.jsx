a0399290539185e9a4a8a7b45cd5a259d0335b87
Karthik Vishal <karthikvishal1506@gmail.com>
2025-07-06 11:10:26 +0530

    showbooks http: changed to https:...

--- Frontend/src/pages/ShowBooks.jsx
+++ Frontend/src/pages/ShowBooks.jsx
@@ -12,7 +12,7 @@ const ShowBooks = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`http://book-store-backend-jet.vercel.app/books/${id}`)
+      .get(`https://book-store-backend-jet.vercel.app/books/${id}`)
       .then((response) => {
         setBook(response.data);
         setLoading(false);
